c3c1f1bf28ae0733419995804cbc217ab424c53c
ss585801 <ss585801@hotmail.com>
2024-08-21 22:24:57 +0800

    目前完成的階段上傳

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+  spaceBetween: 48,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination-custom",
+    type: "fraction"
+  }
+});
